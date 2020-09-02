cd5e89694a445e9602672383f784ded555eb1937
Willis Stearns <lzrg4life@gmail.com>
2020-09-02 08:23:26 -0400

    Another test

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-(function () {
+(() => {
+    
     const router = new Router(document.getElementById('app'), [
         new Route('home', 'home.html'),
         new Route('about', 'about.html')
