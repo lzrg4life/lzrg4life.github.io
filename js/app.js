689d6f5aa778df5e5a1ac3767c02b2f4dda10e7a
Willis Stearns <52726855+lzrg4life@users.noreply.github.com>
2023-03-20 21:15:30 -0400

    Add grocery route

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 (() => {
     const router = new Router(document.getElementById('app'), [
         new Route('home', 'home.html'),
+        new Route('grocery', 'grocery.html'),
         new Route('about', 'about.html')
     ]);
 
